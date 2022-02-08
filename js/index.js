4a6a25ef15f8ce682eb6cb8301f151a2cf4ce5aa
D-Rose04 <davidbhr04@gmail.com>
2022-02-07 22:23:00 -0400

    first commit

--- js/index.js
+++ js/index.js
@@ -1,13 +1,96 @@
-function getValue(val){
-    document.calc.screen.value+=val;
+class Calculator{
+    constructor(prevText,currentText){
+        this.prevText = prevText;
+        this.currentText = currentText;
+        this.current = "";
+        this.prev = "";
+    }
+
+    setNumber(number){
+        if (this.current.includes(".") && number === ".") return;
+        this.current += number.toString();    
+    }
+    
+    setOperator(operator){
+        if(this.current === "") return;
+        if(this.prev !=="") this.getResult();
+        this.operator = operator;
+        this.prev = this.current + this.operator;
+        this.current = "";
+        
+    }
+
+    getResult(){
+        let operation = this.prev + this.current;
+        this.prev = operation + "=";
+        this.current = eval(operation);
+    }
+
+    clearDisplay(){
+        this.operator = undefined;
+        this.prev = "";
+        this.current = "";
+    }
+
+    delDisplay(){
+        this.current = this.current.slice(0,-1);
+    }
+    
+    updateDisplay(){
+        this.currentText.innerHTML = this.current;
+        this.prevText.innerHTML = this.prev;
+    }
 }
 
-function result(){
-    let op = document.calc.screen.value;
-    document.calc.screen.value = eval(op);
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equals");
+const del = document.querySelector(".del");
+const clear = document.querySelector(".clear");
+const prev = document.querySelector("#prev");
+const current = document.querySelector("#current");
+const history = document.querySelector(".history");
+const calculator = new Calculator(prev,current);
+
+numbers.forEach(number => {
+    number.addEventListener("click", ()=>{
+        calculator.setNumber(number.innerHTML);
+        calculator.updateDisplay();
+    });
+});
+
+operators.forEach(operator => {
+    operator.addEventListener("click", ()=>{
+        calculator.setOperator(operator.innerHTML);
+        calculator.updateDisplay();
+    });
+});
+
+equals.addEventListener("click", () => {
+    calculator.getResult();
+    calculator.updateDisplay();
+});
+
+clear.addEventListener("click", () => {
+    calculator.clearDisplay();
+    calculator.updateDisplay();
+});
+
+del.addEventListener("click", () => {
+    calculator.delDisplay();
+    calculator.updateDisplay();
+    toggle();
+});
+
+function toggle(){
+    var x = document.getElementById("historyDisplay");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
 }
 
-function del(){
-    let screen = document.calc.screen.value;
-    document.calc.screen.value = screen.slice(0,-1)
-}   
+history.addEventListener("click", ()=>{
+    toggle();
+});
